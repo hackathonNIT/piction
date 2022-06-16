1aafde624cf9b530f221399a35c0db627a189108
koouu <kotatata420@gmail.com>
2022-06-17 01:34:39 +0900

    ディレクトリ構成変更

---
+++ README.md
@@ -0,0 +1,1 @@
+#Piction  
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name="piction",
+	version="1.0",
+	description="お絵描き関数変換アプリ",
+	auther="hackathonNIT",
+	url="https://github.com/hackathonNIT/piction",
+)
