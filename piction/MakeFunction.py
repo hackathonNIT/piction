b53c14f00da217e4be82c6caf1c73fc9999a0433
nakaji-nandaina <e8571@g.maizuru-ct.ac.jp>
2022-06-17 17:24:38 +0900

    add makefunction

---
+++ piction/MakeFunction.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+x=[]
+y1=[]
+popt=[]
+
+def func1(X, *params):
+    Y = np.zeros_like(X)
+    for i, param in enumerate(params):
+        Y = Y + np.array(param*(np.sin(i*i* X )))
+    return Y
+
+
+def WriteFunc1():
+    Y = ""
+    for i,param in enumerate(popt):
+        if i!=0:
+          if param >= 0:
+            Y = Y + "+"
+          Y=Y+str(param)+"*"+"sin("+str(i*i)+"x)"
+    return Y
+
+
+def getSinRegression(X,Y1):
+
+    x=np.array(X)
+    y1=np.array(Y1)
+    x=x.astype(float)
+    y1=y1.astype(float)
+    plt.figure("picture function")
+
+    popt, pcov=curve_fit(func1,x, y1, p0=[1]*len(x))
+
+def plotRegression(){
+    plt.plot(x,func1(x,*popt),label="RegressionFunction")
+}
