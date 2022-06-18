f4628968b3b1569c43678b01a19ce9317dde49f1
nakaji-nandaina <e8571@g.maizuru-ct.ac.jp>
2022-06-18 10:49:42 +0900

    return x,y

--- piction/MakeFunction.py
+++ piction/MakeFunction.py
@@ -42,8 +42,8 @@ def getSinRegression(X,Y1):
     print(x)
     global popt
     popt, pcov=curve_fit(func1,x, y1, p0=[1]*len(x))
-    Y1=func1(X, *popt)
-    return x,Y1
+    y=func1(X, *popt)
+    return x,y
     #popt, pcov=curve_fit(func2,x, y1, p0=[0]*len(x))
 
 def plotRegression():
